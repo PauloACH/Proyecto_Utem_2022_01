3d81906caf4a61c8ff91c863a8b7c132f6cbe5f5
Paulo Vera <51829447+PauloACH@users.noreply.github.com>
2022-04-30 12:40:38 -0400

    Actualizacion realizada en clases

--- app/src/main/java/com/example/proyecto_utem_2022_01/MainActivity.java
+++ app/src/main/java/com/example/proyecto_utem_2022_01/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.proyecto_utem_2022_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSiguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSiguiente = findViewById(R.id.button);
+
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("MainActivity", "onClick");
+                Intent intent = new Intent(MainActivity.this, DosActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
